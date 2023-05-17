bef09e7625ce357789cddfb302974b3846a81a15
pd <pd>
2023-05-17 19:36:31 +0800

    1.0.5

--- server/app.js
+++ server/app.js
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+var cors=require('cors')
+app.use(cors('http://47.97.22.215'))
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
